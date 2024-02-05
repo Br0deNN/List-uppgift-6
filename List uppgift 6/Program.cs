10f6265264f5c6572ebb55ac69ffa163f5c7002e
Br0deNN <theo.broden@gmail.com>
2024-02-05 20:10:05 +0100

    Add project files.

---
+++ List uppgift 6/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Dictionary<string, int> priser = new Dictionary<string, int>();
+
+        Console.WriteLine("Skriv in namn på matvaror");
+        string[] matvaror = Console.ReadLine().Split();
+
+        Console.WriteLine("Skriv in matvarornas pris");
+        string[] matpriser = Console.ReadLine().Split();
+
+        Console.WriteLine("Skriv in din handlingslista");
+        string[] shoplist = Console.ReadLine().Split();
+
+        for (int i = 0; i < matvaror.Length; i++)
+        {
+            priser[matvaror[i]] = int.Parse(matpriser[i]);
+        }
+
+        int sum = 0;
+        for (int i = 0;i < shoplist.Length; i++)
+        {
+            sum += priser[shoplist[i]];
+        }
+        Console.WriteLine($"Du behöver betala {sum} kr, för dina matvaror");
+    }
+}
